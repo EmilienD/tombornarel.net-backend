a402e156ad50e514235e26d4b80e2f0b1a0ddc79
Emilien Durieu <contact@emilienduri.eu>
2017-10-01 14:24:08 +0200

    use body parser

--- source/index.mjs
+++ source/index.mjs
@@ -1,10 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 import controllers from './controllers'
 
 const tommyBornyFabulousWebsitey = express()
 
-tommyBornyFabulousWebsitey.use('/',
-controllers)
+tommyBornyFabulousWebsitey.use(
+  '/',
+  bodyParser.json(),
+  controllers
+)
 
 tommyBornyFabulousWebsitey.listen(6666)
