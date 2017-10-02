57152f37cc26a3c3a63f3bf39432a84cc947f3b4
Emilien Durieu <contact@emilienduri.eu>
2017-10-02 20:58:20 +0200

    add validation to entity constructors

--- source/models/Gallery.mjs
+++ source/models/Gallery.mjs
@@ -1,5 +1,13 @@
+import {EntityConstructionError} from '../errors'
+
 class Gallery {
-  constructor({name, projects, index}) {
+  constructor(galleryDto) {
+    if (galleryDto.images !== undefined
+      || galleryDto.description !== undefined){
+      throw new EntityConstructionError({target:'Gallery', suspect:'Project'})
+    }
+
+    const {name, projects, index} = galleryDto
     this.name = name || ''
     this.projects = projects || []
     this.index = typeof index === 'number' ? index : null
--- source/models/Project.mjs
+++ source/models/Project.mjs
@@ -1,9 +1,39 @@
+import {EntityConstructionError, InvalidEntityError} from '../errors'
+
+function isAnImage({filename, index}) {
+  return typeof filename === 'string' && typeof index === 'number'
+}
+
+function isNotAnImage(image) { return !isAnImage(image)}
+
+function projectValidator(projectDto) {
+  if (projectDto.projects !== undefined) {
+    throw new EntityConstructionError({
+      target: 'Project',
+      suspect: 'Gallery',
+    })
+  }
+
+  const { name, images } = projectDto
+  if(!name){ throw new InvalidEntityError({
+    entityName: 'Project',
+    cause: 'projects must have a name'
+  })}
+  if(images.some(isNotAnImage)){ throw new InvalidEntityError({
+    entityName: 'Project',
+    cause: 'image array is wrong'
+  }) }
+}
+
 class Project {
-  constructor() {
-    this.name = ''
-    this.description = ''
-    this.images = []
-    this.index = null
+  constructor(projectDto) {
+    projectValidator(projectDto)
+    
+    const { name, description, images, index } = projectDto
+    this.name = name
+    this.description = description
+    this.images = images
+    this.index = index
   }
 }
 
