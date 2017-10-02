cb875005d9f29395b0a3d367b78dc43fbbee112a
Emilien Durieu <contact@emilienduri.eu>
2017-10-02 21:41:10 +0200

    add error handler

--- source/index.mjs
+++ source/index.mjs
@@ -6,7 +6,17 @@ import controllers from './controllers'
 const tommyBornyFabulousWebsitey = express()
 
 tommyBornyFabulousWebsitey.use(
-  '/',
+  (req, res, next) => {
+    try {
+      next()
+    } catch(e) {
+      if (e.satus && e.httpMessage) {
+        res.status(e.status).send('Sorry, an error happened:', e.httpMessage)
+      } else {
+        res.status(500).send('Aaah! Caffeine overdose!')
+      }
+    }
+  },
   bodyParser.json({type: 'application/json'}),
   controllers
 )
